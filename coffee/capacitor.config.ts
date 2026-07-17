ce36c97bff6809bc8d449893dea7e4f212f49ff5
magnvsus <magnvsus@gmail.com>
2026-07-16 21:54:56 -0400

    init: capacitor init files

---
+++ coffee/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.Group25.InventoryHub',
+  appName: 'InventoryHub',
+  webDir: 'dist'
+};
+
+export default config;
